5cc5b0f425a7b63469433397844df0bd5ee2967e
JaagupA <jaagup.averin@gmail.com>
2025-08-05 18:36:16 +0300

    HMMM

---
+++ net_app_relay/src/main.cpp
@@ -0,0 +1,16 @@
+#include <app_version.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+int main() {
+	LOG_INF("Hello, world?! " APP_VERSION_STRING);
+
+	int i = 0;
+	while (true) {
+		LOG_INF("TICK #%d!", i++);
+		k_sleep(K_MSEC(10000));
+	}
+	return 0;
+}
